7f73a5aba0664a380a2000655d0e20ad121e75a1
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-07-23 00:41:41 -0400

    refactored weather tomorrow

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# weather-slackbot
+# Weather SlackBot 
+
+### Requirements:
+
+- [Slack-Ruby-Bot](https://github.com/slack-ruby/slack-ruby-bot#slack-ruby-bot): framework used for automated SlackBot responses.
+- [Whenever](https://github.com/javan/whenever): gem used to build scheduled tasks
+
+### Getting Started
+
+Once
--- siobhan_bot/commands/weather_tomorrow.rb
+++ siobhan_bot/commands/weather_tomorrow.rb
@@ -6,16 +6,37 @@ require 'config/api_keys'
 module SiobhanBot
   module Commands
     class WeatherTomorrow < SlackRubyBot::Commands::Base
-      command "weather tomorrow" do |client, data, _match|
-        tomorrowDate = Time.new(Time.now.year, Time.now.month, (Time.now.day + 1)).to_i
-        response = JSON.parse(RestClient.get("https://api.darksky.net/forecast/#{DARK_SKY_API_KEY}/40.710740,-74.007032,#{tomorrowDate}?exclude=minutely,hourly/"))["daily"]["data"][0]
-        summary = response["summary"]
-        maxTemp = (response["temperatureMax"]).to_i
-        maxTempTime = "#{Time.at(response["temperatureMaxTime"]).strftime("%l:%M %P")}"
-        minTemp =  (response["temperatureMin"]).to_i
-        minTempTime = "#{Time.at(response["temperatureMinTime"]).strftime("%l:%M %P")}"
-        client.say(text: "#{summary} Expect a high of #{maxTemp}° at #{maxTempTime} and a low of #{minTemp}° at #{minTempTime}.", channel: data.channel)
 
+      command "weather tomorrow" do |client, data, _match|
+        client.say(text: "#{summary} Expect a high of #{max_temp}° at #{max_temp_time} and a low of #{min_temp}° at #{min_temp_time}.", channel: data.channel)
+      end
+
+      def self.tomorrow_date
+        Time.new(Time.now.year, Time.now.month, (Time.now.day + 1)).to_i
+      end
+
+      def self.api_response
+         JSON.parse(RestClient.get("https://api.darksky.net/forecast/#{DARK_SKY_API_KEY}/40.710740,-74.007032,#{tomorrow_date}?exclude=minutely,hourly/"))["daily"]["data"][0]
+      end
+
+      def self.summary
+        api_response["summary"]
+      end
+
+      def self.max_temp
+        (api_response["temperatureMax"]).to_i
+      end
+
+      def self.max_temp_time
+        "#{Time.at(api_response["temperatureMaxTime"]).strftime("%l:%M %P")}"
+      end
+
+      def self.min_temp
+        (api_response["temperatureMin"]).to_i
+      end
+
+      def self.min_temp_time
+        "#{Time.at(api_response["temperatureMinTime"]).strftime("%l:%M %P")}"
       end
 
     end
